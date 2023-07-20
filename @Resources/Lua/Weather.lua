2ec23e95eb819b5097fca7241ce7ca8a178f803f
max3pro <max3win@outlook.com>
2023-07-20 08:09:07 +0300

    Remove .lua errors

--- @Resources/Lua/Weather.lua
+++ @Resources/Lua/Weather.lua
@@ -1,18 +1,37 @@
 function Initialize()
-	jsonLibPath = SKIN:GetVariable('@')..'Lua\\json.lua'
-	JSON = assert(loadfile(SKIN:GetVariable('@')..'Lua\\json.lua'))()
-    JSON.strictTypes = false
-    JSON.strictParsing = false
+	dofile( SKIN:GetVariable( '@' )..'Lua\\json.lua' )
+	measure = SKIN:GetMeasure( "MeasureGetData" )
+	first_run = true
 end
 
 
 function Update()
-	measureJson = SKIN:GetMeasure('measureGetJson')
-	-- measureJsonValue = measureJson:GetStringValue()
-	key = JSON:decode(measureJson:GetStringValue())
+	-- We have no data to parse on first run, 
+	-- because WebParser(MeasureGetData) doesn't retrive API data yet on first run (Initilize also started)
+	if first_run then
+		first_run = false
+		print( 'First run...')
+		return
+	end
 
-	SKIN:Bang( '!SetVariable', 'temperature', key.current_weather.temperature )
-	SKIN:Bang( '!SetVariable', 'windSpeed', key.current_weather.windspeed )
-	SKIN:Bang( '!SetVariable', 'weatherCode', key.current_weather.weathercode )
-	-- return tostring(key.current_weather.temperature)
+	json_string = measure:GetStringValue()
+
+	if not json_string or string.len( json_string ) == 0 then
+		print( "Error: Weather API returned: " .. json_string )
+		return
+	end
+
+	local success, weather_data = pcall( decodeData, json_string )
+	if not success then
+		print( "Error from weather API: " .. ret )
+		return
+	end
+
+	return tostring( weather_data.current_weather.temperature ) .. '  ' .. tostring( weather_data.current_weather.windspeed )
+end
+
+
+function decodeData( json_string )
+	print( json_string )
+	return json_decode( json_string )
 end
