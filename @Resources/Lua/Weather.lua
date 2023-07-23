3dc02f2f05e831e842987df24b0ea0b9bbbd38c6
max3pro <max3win@outlook.com>
2023-07-23 09:43:26 +0300

    Design improvements

--- @Resources/Lua/Weather.lua
+++ @Resources/Lua/Weather.lua
@@ -24,15 +24,16 @@ function Update()
 	SKIN:Bang( '!SetVariable', 'weather_icon', weather.weather[1].icon )
 	SKIN:Bang( '!SetVariable', 'weather_description1', weather.weather[1].description:sub(1,1) )
 	SKIN:Bang( '!SetVariable', 'weather_description2', weather.weather[1].description:sub(2) )
-	SKIN:Bang( '!SetVariable', 'current_temperature', weather.main.temp )
+	SKIN:Bang( '!SetVariable', 'temperature', weather.main.temp )
+	SKIN:Bang( '!SetVariable', 'temperature_feels', weather.main.feels_like )
 	SKIN:Bang( '!SetVariable', 'wind_speed', weather.wind.speed )
+	SKIN:Bang( '!SetVariable', 'wind_gust', weather.wind.gust )
+	SKIN:Bang( '!SetVariable', 'wind_degrees', weather.wind.deg )
 	SKIN:Bang( '!SetVariable', 'pressure', weather.main.grnd_level )
 	SKIN:Bang( '!SetVariable', 'humidity', weather.main.humidity )
 	SKIN:Bang( '!SetVariable', 'cloudiness', weather.clouds.all )
 	SKIN:Bang( '!SetOption', 'MeasureSunrise', 'TimeStamp', weather.sys.sunrise + weather.timezone)
 	SKIN:Bang( '!SetOption', 'MeasureSunset', 'TimeStamp', weather.sys.sunset + weather.timezone)
-
-	return weather.weather[1].icon
 end
 
 
